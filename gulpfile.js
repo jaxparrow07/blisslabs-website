5d3bc62851fc1554c21f30f0475f5fba70decd29
jaxparrow07 <zinczorphin@gmail.com>
2024-01-03 01:56:27 +0530

    refactor: auto build site

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,64 @@
-var gulp    = require('gulp'),
-    less    = require('gulp-less'),
-    connect = require('gulp-connect'),
-    pug     = require('gulp-pug'),
-    plumber = require('gulp-plumber'),
-    rename  = require('gulp-rename'),
+var gulp        = require('gulp'),
+    less        = require('gulp-less'),
+    connect     = require('gulp-connect'),
+    pug         = require('gulp-pug'),
+    plumber     = require('gulp-plumber'),
+    rename      = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer');
 
 function reload(done) {
-  connect.server({
-    livereload: true,
-    port: 5000
-  });
-  done();
+    connect.server({
+        livereload: true,
+        port: 5000
+    });
+    done();
 }
 
 function styles() {
-  return (
-    gulp.src('src/less/styles.less')
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 3 versions'],
-      cascade: false
-    }))
-    .pipe(less({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('assets/css'))
-    .pipe(less({outputStyle: 'compressed'}))
-    .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest('assets/css'))
-    .pipe(connect.reload())
-  );
+    return (
+        gulp.src('src/less/styles.less')
+            .pipe(plumber())
+            .pipe(less())
+            .pipe(autoprefixer({
+                overrideBrowserslist: ['last 3 versions'],
+                cascade: false
+            }))
+            .pipe(less({ outputStyle: 'expanded' }))
+            .pipe(gulp.dest('assets/css'))
+            .pipe(less({ outputStyle: 'compressed' }))
+            .pipe(rename('styles.min.css'))
+            .pipe(gulp.dest('assets/css'))
+            .pipe(connect.reload())
+    );
 }
 
 function html() {
-  return (
-    gulp.src('*.html')
-    .pipe(plumber())
-    .pipe(connect.reload())
-  );
+    return (
+        gulp.src('*.html')
+            .pipe(plumber())
+            .pipe(connect.reload())
+    );
 }
 
 function views() {
-  return (
-    gulp.src('src/pug/pages/*.pug')
-    .pipe(plumber())
-    .pipe(pug({
-        blade: false,
-        pretty: true,
-        search: 'src/pug/**/*.pug'
-    }))
-    .pipe(gulp.dest('./'))
-    .pipe(connect.reload())
-  )
+    return (
+        gulp.src('src/pug/pages/*.pug')
+            .pipe(plumber())
+            .pipe(pug({
+                blade: false,
+                pretty: true,
+                basedir: 'src/pug', // Specify the base directory for Pug includes
+            }))
+            .pipe(gulp.dest('./'))
+            .pipe(connect.reload())
+    )
 }
 
 function watchTask(done) {
-  gulp.watch('*.html', html);
-  gulp.watch('src/less/**/*.less', styles);
-  gulp.watch('src/pug/**/*.pug', views);
-  done();
+    gulp.watch('*.html', html);
+    gulp.watch('src/less/**/*.less', styles);
+    gulp.watch('src/pug/**/*.pug', views);
+    done();
 }
 
 const watch = gulp.parallel(watchTask, reload);
